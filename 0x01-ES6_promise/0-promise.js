bf23c360ba3670cfce48187ffd57c428773279dd
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-06-25 17:22:36 +0100

    Create 0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous operation
+    resolve('Success');
+  });
+}
+
+export default getResponseFromAPI;
