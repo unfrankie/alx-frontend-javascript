1c473e4b8159d6e14b21718199c33b2ea8cd930a
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-06-25 17:55:37 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,8 @@
-export default function getResponseFromAPI() {
-  return (new Promise((resolve, reject) => {}));
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous operation
+    resolve('Success');
+  });
 }
+
+export default getResponseFromAPI;
