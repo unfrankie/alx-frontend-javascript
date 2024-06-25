23d6e68045c00b65e4f2ae3f287b2db91a3a0875
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-06-25 17:26:11 +0100

    Create 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason
+    }));
+  });
+}
+
+export default handleProfileSignup;
