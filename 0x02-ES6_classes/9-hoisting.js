9f343688b0ea5c4da37bb1944d0c75a0f57a32dc
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-06-28 00:47:17 +0100

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,4 @@
-export class HolbertonClass {
+class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -13,7 +13,7 @@ export class HolbertonClass {
   }
 }
 
-export class StudentHolberton {
+class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -42,4 +42,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export { listOfStudents as default, HolbertonClass, StudentHolberton };
