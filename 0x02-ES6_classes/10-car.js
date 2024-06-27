b05b7afd37e4c5ad64dec8e52f123c5b6c85fbcc
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-06-28 00:28:46 +0100

    Create 10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+const cloneSymbol = Symbol('clone');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  [cloneSymbol]() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
