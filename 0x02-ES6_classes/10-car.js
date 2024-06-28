1bc0d75ed568862d41d2b58596714e0789a1ccb6
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-06-28 01:37:17 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,12 @@
-const CarSymbol = Symbol('Car');
-
-class Car {
-  constructor(brand, motor, color) {
+export default class Car {
+  constructor(brand = null, motor = null, color = null) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    const NewCarClass = this.constructor[CarSymbol];
-    return new NewCarClass();
+    // Basic implementation just for demonstration
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
-
-Car[CarSymbol] = Car;
-
-export default Car;
