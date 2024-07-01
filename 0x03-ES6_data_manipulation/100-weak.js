05310f88dcb914acc1a00b3524f010ea62f42d21
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-07-01 17:43:59 +0100

    Create 100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const count = weakMap.get(endpoint) + 1;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, count);
+}
