5c340e51ec220732f2471129d466181cb5e526d9
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-07-03 22:52:57 +0100

    Update interface.ts

--- 0x04-TypeScript/task_3/js/interface.ts
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -5,14 +5,3 @@ export interface RowElement {
     lastName: string;
     age?: number;
 }
-task_3/js/crud.d.ts
-
-typescript
-Copy code
-import { RowID, RowElement } from './interface';
-
-declare module 'crud' {
-    export function insertRow(row: RowElement): RowID;
-    export function deleteRow(rowId: RowID): void;
-    export function updateRow(rowId: RowID, row: RowElement): RowID;
-}
